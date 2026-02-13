ea040b3cd9d8998ab85f10c783065d30fc0c9062
saninduhansara <e22130@eng.pdn.ac.lk>
2026-02-13 22:47:43 +0530

    fixed product.js

--- models/product.js
+++ models/product.js
@@ -1,53 +1,50 @@
 import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
-
     productId : {
         type : String,
         required : true,
         unique : true
     },
-
     name : {
         type : String,
         required : true
     },
-
     altNames : {
         type : [String],
         default : []
     },
-
     labelledPrice : {
         type : Number,
         required : true
     },
-
+    price : {
+        type : Number,
+        required : true
+    },
     images : {
         type : [String],
-        default : ["default-product.jpg"]
+        default : ["/default-product.jpg"]
     },
-
     description : {
         type : String,
-        required : true,
-        default : ""
+        required : true
     },
-
+    stock : {
+        type : Number,
+        required : true,
+        default : 0
+    },
     isAvailable : {
         type : Boolean,
         default : true
     },
-
     category : {
         type : String,
         required : true,
-        default : "cosmetics"
-
+        default : "cosmatics"
     }
-
-    
 })
 
-const Product = mongoose.model('Products', productSchema);
+const Product = mongoose.model("products",productSchema)
 export default Product;
